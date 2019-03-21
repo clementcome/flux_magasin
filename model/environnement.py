69048f3db3583ae57c83961566327607cb87591d
Clément Côme <clement.come@student-cs.fr>
2019-03-21 11:44:25 +0100

    Intro class

--- model/environnement.py
+++ model/environnement.py
@@ -4,4 +4,15 @@ class Mur():
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-mur1 = Mur(0,0,0,1)
+
+class Meuble():
+    def __init__(self,x1,y1,x2,y2,x3,y3,x4,y4,attraction):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+        self.x4 = x4
+        self.y4 = y4
+        self.attraction = attraction
