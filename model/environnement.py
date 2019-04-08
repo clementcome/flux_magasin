7820100dfe5c5d60ed648b995c8025828be02a70
Corentin <corentin.laugier@student-cs.fr>
2019-04-08 22:12:39 +0200

    branche

--- model/environnement.py
+++ model/environnement.py
@@ -1,9 +1,15 @@
+from math import inf
+
 class Mur:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+
+    @property
+    def getCoordinates(self):
+        return [self.x1,self.y1,self.x2,self.y2]
 
 
 class Meuble:
@@ -14,8 +20,94 @@ class Meuble:
         self.y2 = y2
 
     @property
+    def getPos(self):
+        return [self.x1,self.y1,self.x2,self.y2]
+
     def getCenter(self):
-        return (x1 + x2) / 2, (y1 + y2) / 2
+        return (self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2
 
+    def inside(self,pos): # on regarde si un point est dans le meuble (il faut implémenter un meuble intérieur (
+        # probablement en rajoutant des murs) ou les gens ne peuvent pas rentrer
+        if pos[0]>self.x1 and pos[0]<self.x2 and pos[1]>self.y1 and pos[1]<self.y2:
+            return True
+        else:
+            return False
 
-mur1 = Mur(0, 0, 0, 1)
+class Client:
+    def __init__(self, x, y, v_x, v_y, tRemain, nbRemain=None, attractCoef=None):
+        self.x = x
+        self.y = y
+        self.v_x = v_x
+        self.v_y = v_y
+        self.t_remain = tRemain
+        self.nb_remain = nbRemain
+        self.leaving = False
+
+        if nbRemain is None:
+            self.nb_remain = inf
+        else:
+            self.nb_remain = nbRemain
+
+        if attractCoef is None:
+            self.attract_coef = 1
+        else:
+            self.attract_coef = attractCoef
+    @property
+    def getPos(self):
+        return [self.x,self.y]
+
+    def getSpeed(self):
+        return [self.v_x, self.v_y]
+
+    def getTime(self):
+        return self.t_remain
+
+    def getArticle(self):
+        return self.nb_remain
+
+    def getAttract(self):
+        return self.attract_coef
+
+    def setPos(self,pos):
+        self.x = pos[0]
+        self.y = pos[1]
+
+    def setSpeed(self, speed):
+        self.v_x = speed[0]
+        self.v_y = speed[1]
+
+    def updateTime(self,time_passed): #on enlève le temps qui s'est écoulé
+        self.t_remain = self.t_remain - time_passed
+        if self.t_remain < 0:
+            self.leaving = True
+
+    def updateArticle(self):
+        self.nb_remain+=-1
+
+class Entry:
+    def __init__(self,x1,y1,x2,y2,flow):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.flow = flow
+
+    @property
+
+    def getPos(self):
+        return [self.x1,self.y1,self.x2,self.y2]
+
+    def getFlow(self):
+        return self.flow
+
+class Exit:
+    def __init__(self,x1,y1,x2,y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    @property
+
+    def getPos(self):
+        return [self.x1,self.y1,self.x2,self.y2]
--- model/evolution.py
+++ model/evolution.py
@@ -1,1 +1,5 @@
-import environnement
+from flux_magasin.model.environnement import * #importé de cette façon et pas avec import flux_magasin.model.environnement, qui rendrait claque appel de mur illisible
+
+mur1 = Mur(1,2,3,4)
+
+print(mur1.getCoordinates)
