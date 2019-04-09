a41240ff7148a3640c85d695d6146d2acef50cb4
Corentin <corentin.laugier@student-cs.fr>
2019-04-09 19:02:00 +0200

    plusieurs entrées/sorties

--- model/evolution.py
+++ model/evolution.py
@@ -1,6 +1,7 @@
 from flux_magasin.model.environnement import * #importé de cette façon et pas avec import flux_magasin.model.environnement, qui rendrait claque appel de mur illisible
 import matplotlib.pyplot as plt
 
+
 wall1 = Wall(1,2,3,4)
 
 shop = Shop("Zara")
