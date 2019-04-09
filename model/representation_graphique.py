12ba1190eb0a242b61d0bfd256b5a82d8a63ce94
lilasainero <lila.sainero@student-cs.fr>
2019-04-09 18:01:09 +0200

    Changement d'arguments pour les fonctions d'affichage (début)

--- model/representation_graphique.py
+++ model/representation_graphique.py
@@ -1,23 +1,25 @@
 from tkinter import *
 from flux_magasin.model.environnement import *
 
-def creation_fenetre(murs):
+def creation_fenetre(shop):
     global root
     root = Tk()
     root.title('Magasin')
+
+    murs = shop.getWalls
 
     x_max = 0
     y_max = 0
     for mur in murs:
         coord = mur.getPos
         if coord[0] > x_max:
-            x_max =coord[0]
+            x_max = coord[0]
         if coord[2] > x_max:
-            x_max =coord[2]
+            x_max = coord[2]
         if coord[1] > y_max:
-            y_max =coord[1]
+            y_max = coord[1]
         if coord[3] > y_max:
-            y_max =coord[3]
+            y_max = coord[3]
 
     global magasin
     magasin = Canvas(root, width=x_max+15, height=y_max+15)
@@ -57,6 +59,13 @@ Entrees_test = [Entry(0,100,0,150,45)]
 Sorties_test = [Exit(200,0,250,0)]
 Meubles_test = [Stand(0,0,25,50), Stand(150,150,250,180)]
 Clients_test = [Client(45,78,3,4,6), Client(187,23,7,7,7)]
+
+Shop_test = Shop('test')
+for wall in Murs_test:
+    Shop.addWall(wall)
+for stand in Meubles_test:
+    Shop.addStand(stand)
+
 
 creation_fenetre(Murs_test)
 affichage_magasin(Murs_test, Meubles_test, Entrees_test, Sorties_test, magasin)
