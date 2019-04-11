e51f291da62bc289204036812293656a70bf8c0e
Corentin <corentin.laugier@student-cs.fr>
2019-04-11 14:43:10 +0200

    v_max

--- model/field_of_view.py
+++ model/field_of_view.py
@@ -1,5 +1,6 @@
 from flux_magasin.model.environnement import *
 from flux_magasin.model.intersections import *
+from flux_magasin.model.representation_graphique_statique import *
 
 def field_of_view(stand,shop): #pas fini
     center = stand.getCenter()
@@ -37,23 +38,31 @@ def field_of_view(stand,shop): #pas fini
                     poly.append(point)
     return poly
 
-##test
-Murs_test = [Wall(0,0,0,200), Wall(0,200,300,200), Wall(300,200,300,0), Wall(300,0,0,0)]
-Entrees_test = [Entry(200,0,245,0,45), Entry(150,200,180,200,45)]
-Sorties_test = [Exit(0,100,0,150), Exit(150,200,180,200)]
-Meubles_test = [Stand(0,0,25,50), Stand(150,150,250,180)]
-Clients_test = [Client(45,78,-0.4,0.8,6), Client(45,78,-0.4,0.8,6)]
+if __name__ == '__main__':
+
+    Murs_test = [Wall(0,0,0,200), Wall(0,200,300,200), Wall(300,200,300,0), Wall(300,0,0,0)]
+    Entrees_test = [Entry(200,0,245,0,45), Entry(150,200,180,200,45)]
+    Sorties_test = [Exit(0,100,0,150), Exit(150,200,180,200)]
+    Meubles_test = [Stand(0,0,25,50), Stand(150,150,250,180)]
+    Clients_test = [Client(45,78,3,4,6), Client(187,23,7,7,7)]
+
+    Shop_test = Shop('test')
+    for wall in Murs_test:
+        Shop_test.addWall(wall)
+    for stand in Meubles_test:
+        Shop_test.addStand(stand)
+    for entry in Entrees_test:
+        Shop_test.addEntry(entry)
+    for exit in Sorties_test:
+        Shop_test.addExit(exit)
+    for client in Clients_test:
+        Shop_test.addClient(client)
 
 
-Shop_test = Shop('test')
-for wall in Murs_test:
-    Shop_test.addWall(wall)
-for stand in Meubles_test:
-    Shop_test.addStand(stand)
-for entry in Entrees_test:
-    Shop_test.addEntry(entry)
-for exit in Sorties_test:
-    Shop_test.addExit(exit)
-for client in Clients_test:
-    Shop_test.addClient(client)
-print(field_of_view(Shop_test.getStands()[1],Shop_test)) #on voit que ça marche pas trop
+    creation_fenetre(Shop_test)
+    affichage_magasin(Shop_test, magasin)
+    affichage_clients(Shop_test, magasin)
+    root.mainloop()
+
+    creation_legende()
+
